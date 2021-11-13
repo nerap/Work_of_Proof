64594362b8380ea6aeed4db4452586e15a269943
Nerap <raph.en223@gmail.com>
2021-11-13 20:47:28 +0100

    Adding wallet

--- srcs/main.py
+++ srcs/main.py
@@ -1,6 +1,8 @@
-import binascii
-import rsa
+from wallet import Wallet 
 
 if __name__ == "__main__":
 	print("hello")
+	wall = Wallet.Wallet.create()
+	print(wall.address)
+	print(wall.priv)
 	pass
